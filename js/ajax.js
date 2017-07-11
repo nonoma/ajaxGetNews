aa0755b70384f0c18a8f6d95340078898dcbf4f4
nonoma <malanqingnono@163.com>
2017-07-11 18:43:43 +0800

    Initial commit

---
+++ js/ajax.js
@@ -0,0 +1,44 @@
+    function ajax(obj) {
+        var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+        obj.url += '?' + obj.data;
+        xhr.open(obj.method, obj.url, true);
+        if (obj.method == 'get') {
+            xhr.send();
+        } else if (obj.method == 'post') {
+            xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded');
+            xhr.send(data);
+        } else {
+        	alert("the method is wrong!!");
+        }
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4) {
+                if (xhr.status == 200) {
+                	obj.success && obj.success(xhr.responseText);
+                }else {
+                	alert('the error is' + xhr.status);
+                }
+            }
+        }
+    }
+    //     function ajax(method, url, data, success) {
+    //     var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+    //     url += '?' + data;
+    //     xhr.open(method, url, true);
+    //     if (method == 'get') {
+    //         xhr.send();
+    //     } else if (method == 'post') {
+    //         xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded');
+    //         xhr.send(data);
+    //     } else {
+    //         alert("the method is wrong!!");
+    //     }
+    //     xhr.onreadystatechange = function() {
+    //         if (xhr.readyState == 4) {
+    //             if (xhr.status == 200) {
+    //                 success && success(xhr.responseText);
+    //             }else {
+    //                 alert('the error is' + xhr.status);
+    //             }
+    //         }
+    //     }
+    // }
